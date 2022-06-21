2599486f1ca43d76283f369cdfc0f3e5148d0f05
krish-98 <muralikrishnanm98@gmail.com>
2022-06-21 13:37:47 +0530

    setting payload as a naming covention in redux

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const CAKE_RESTOCKED = "CAKE_RESTOCKED"
 function orderCake() {
   return {
     type: CAKE_ORDERED,
-    quantity: 1,
+    payload: 1,
   }
 }
 
 function restockCake(qty = 1) {
   return {
     type: CAKE_RESTOCKED,
-    quantity: qty,
+    payload: qty,
   }
 }
 
@@ -36,7 +36,7 @@ const reducer = (state = initialState, action) => {
     case CAKE_RESTOCKED:
       return {
         ...state,
-        numOfCakes: state.numOfCakes + action.quantity,
+        numOfCakes: state.numOfCakes + action.payload,
       }
     default:
       return state
