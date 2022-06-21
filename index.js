3e1e8528da97037e7ae5622fa69c237a45be6eaf
krish-98 <muralikrishnanm98@gmail.com>
2022-06-21 13:35:12 +0530

    restocking cakes

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const redux = require("redux")
 const createStore = redux.createStore
 
-// object type
+// action type
 const CAKE_ORDERED = "CAKE_ORDERED"
+const CAKE_RESTOCKED = "CAKE_RESTOCKED"
 
 // action creator fn
 function orderCake() {
-  // action type
   return {
     type: CAKE_ORDERED,
+    quantity: 1,
+  }
+}
+
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    quantity: qty,
   }
 }
 
@@ -22,7 +30,13 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
       return {
+        ...state,
         numOfCakes: state.numOfCakes - 1,
+      }
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.quantity,
       }
     default:
       return state
@@ -46,6 +60,8 @@ const unSubcribe = store.subscribe(() =>
 store.dispatch(orderCake())
 store.dispatch(orderCake())
 store.dispatch(orderCake())
+
+store.dispatch(restockCake(3))
 
 //Handles unregistering of listeners via the fn returned by subscribe(listener)
 unSubcribe()
